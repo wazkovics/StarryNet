dd0475a94fc604e23adff4ad5cb6fc547051d5f3
ferenc <161438972+wazkovics@users.noreply.github.com>
2025-05-01 18:11:08 +0100

    New scenario

--- example.py
+++ example.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     # The node index sequence is: 25 sattelites, 2 ground stations.
     # In this example, 25 satellites and 2 ground stations are one AS.
 
-    AS = [[1, 27]]  # Node #1 to Node #27 are within the same AS.
-    GS_lat_long = [[50.110924, 8.682127], [46.635700, 14.311817]
+    AS = [[1, 29]]  # Node #1 to Node #27 are within the same AS.
+    GS_lat_long = [[50.110924, 8.682127], [46.635700, 14.311817],[51.110924, 8.682127],[50.110924, 7.682127]
                    ]  # latitude and longitude of frankfurt and  Austria
     configuration_file_path = "./config.json"
     hello_interval = 1  # hello_interval(s) in OSPF. 1-200 are supported.
@@ -27,70 +27,70 @@ if __name__ == "__main__":
     sn.run_routing_deamon()
     sn.run_webserver_setup()
 
-    node_index1 = 1
-    node_index2 = 2
-    time_index = 2
-
-    # distance between nodes at a certain time
-    node_distance = sn.get_distance(node_index1, node_index2, time_index)
-    print("node_distance (km): " + str(node_distance))
-
-    # neighbor node indexes of node at a certain time
-    neighbors_index = sn.get_neighbors(node_index1, time_index)
-    print("neighbors_index: " + str(neighbors_index))
-
-    # GS connected to the node at a certain time
-    node_index1 = 7
-    GSes = sn.get_GSes(node_index1, time_index)
-    print("GSes are: " + str(GSes))
-
-    # LLA of a node at a certain time
-    LLA = sn.get_position(node_index1, time_index)
-    print("LLA: " + str(LLA))
-
-    sn.get_utility(time_index)  # CPU and memory useage
-
-    # IPList of a node
-    IP_list = sn.get_IP(node_index1)
-    print("IP: " + str(IP_list))
-
-    ratio = 0.3
-    time_index = 5
-    # random damage of a given ratio at a certain time
-    sn.set_damage(ratio, time_index)
-
-    time_index = 10
-    sn.set_recovery(time_index)  # recover the damages at a certain time
-
-    node_index1 = 27
-    time_index = 15
-    # routing table of a node at a certain time. The output file will be written at the working directory.
-    sn.check_routing_table(node_index1, time_index)
-
-    sat = 1
-    des = 27
-    next_hop_sat = 2
-    time_index = 20
-    # set the next hop at a certain time. Sat, Des and NextHopSat are indexes and Sat and NextHopSat are neighbors.
-    sn.set_next_hop(sat, des, next_hop_sat, time_index)
-
-    node_index1 = 13
-    node_index2 = 14
-    time_index = 3
-    # ping msg of two nodes at a certain time. The output file will be written at the working directory.
-    sn.set_ping(node_index1, node_index2, time_index)
-    for i in range(35, 80):
-        node_index1 = 26
-        node_index2 = 27
-        time_index = i
-        # ping msg of two nodes at a certain time. The output file will be written at the working directory.
-        sn.set_ping(node_index1, node_index2, time_index)
-
-    node_index1 = 13
-    node_index2 = 14
-    time_index = 4
-    # perf msg of two nodes at a certain time. The output file will be written at the working directory.
-    sn.set_perf(node_index1, node_index2, time_index)
+    # node_index1 = 1
+    # node_index2 = 2
+    # time_index = 2
+    #
+    # # distance between nodes at a certain time
+    # node_distance = sn.get_distance(node_index1, node_index2, time_index)
+    # print("node_distance (km): " + str(node_distance))
+    #
+    # # neighbor node indexes of node at a certain time
+    # neighbors_index = sn.get_neighbors(node_index1, time_index)
+    # print("neighbors_index: " + str(neighbors_index))
+    #
+    # # GS connected to the node at a certain time
+    # node_index1 = 7
+    # GSes = sn.get_GSes(node_index1, time_index)
+    # print("GSes are: " + str(GSes))
+    #
+    # # LLA of a node at a certain time
+    # LLA = sn.get_position(node_index1, time_index)
+    # print("LLA: " + str(LLA))
+    #
+    # sn.get_utility(time_index)  # CPU and memory useage
+    #
+    # # IPList of a node
+    # IP_list = sn.get_IP(node_index1)
+    # print("IP: " + str(IP_list))
+    #
+    # ratio = 0.3
+    # time_index = 5
+    # # random damage of a given ratio at a certain time
+    # sn.set_damage(ratio, time_index)
+    #
+    # time_index = 10
+    # sn.set_recovery(time_index)  # recover the damages at a certain time
+    #
+    # node_index1 = 27
+    # time_index = 15
+    # # routing table of a node at a certain time. The output file will be written at the working directory.
+    # sn.check_routing_table(node_index1, time_index)
+    #
+    # sat = 1
+    # des = 27
+    # next_hop_sat = 2
+    # time_index = 20
+    # # set the next hop at a certain time. Sat, Des and NextHopSat are indexes and Sat and NextHopSat are neighbors.
+    # sn.set_next_hop(sat, des, next_hop_sat, time_index)
+    #
+    # node_index1 = 13
+    # node_index2 = 14
+    # time_index = 3
+    # # ping msg of two nodes at a certain time. The output file will be written at the working directory.
+    # sn.set_ping(node_index1, node_index2, time_index)
+    # for i in range(35, 80):
+    #     node_index1 = 26
+    #     node_index2 = 27
+    #     time_index = i
+    #     # ping msg of two nodes at a certain time. The output file will be written at the working directory.
+    #     sn.set_ping(node_index1, node_index2, time_index)
+    #
+    # node_index1 = 13
+    # node_index2 = 14
+    # time_index = 4
+    # # perf msg of two nodes at a certain time. The output file will be written at the working directory.
+    # sn.set_perf(node_index1, node_index2, time_index)
 
     sn.start_emulation()
     sn.stop_emulation()
